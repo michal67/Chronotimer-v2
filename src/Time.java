c567096c194606524fc2ce901ce0b3f60297857e
Robert imbrie <robert.m.imbrie@gmail.com>
2017-02-28 13:41:43 -0600

    Create Time.java

---
+++ src/Time.java
@@ -0,0 +1,33 @@
+import java.text.DecimalFormat;
+
+public class Time {
+
+	private long currentTime; // Stores currentTime
+	private long startTime; // Stores TimeStarted
+
+	public Time() {
+		startTime = System.nanoTime();
+	}
+
+	public void ToggleTimer() {
+		startTime = System.nanoTime();
+	}
+
+	public String parseTime() { // Parse current time
+		String result;
+		final double seconds = ((double) this.getTime() / 1000000000);
+		result = new DecimalFormat("#.###").format(seconds) + " Seconds";
+		return result;
+	}
+
+	public String parseTime(long a) { // Parse any integer
+		String result;
+		final double seconds = ((double) a / 1000000000);
+		result = new DecimalFormat("#.###").format(seconds) + " Seconds";
+		return result;
+	}
+
+	public long getTime() {
+		return System.nanoTime() - startTime;
+	}
+}
