5a1f8c5291ac1239df1cdb37e1c3866f4796744e
Robert imbrie <robert.m.imbrie@gmail.com>
2017-02-28 14:08:47 -0600

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -4,17 +4,23 @@ public class Driver {
 		CTController control = new CTController();
 		Simulator simulator = new Simulator();
 		Scanner userPrompt = new Scanner(System.in);
+		boolean fileBeenRead = false;
+		String nextLine;
 		
 		if(!control.controlFromFile()){
 			while(true)
 				simulator.input(userPrompt.nextLine());
 		}
-		else{
+		while(!fileBeenRead){
 			System.out.print("Enter the full file directory: ");
 			try{
 				BufferedReader reader = new BufferedReader(new FileReader(userPrompt.nextLine()));
+				for(String nextLine = reader.nextLine(); nextLine != null; nextLine = reader.nextLine())
+					simulator.input(nextLine);
 			}
-			catch (FileNotFoundException e)
+			catch (FileNotFoundException e){
+				System.out.println("Invalid filepath");
+			}
 		}
 	}
 }
