ce08a5e9946b9398312bdc90c08e3c2dc78ec161
Robert imbrie <robert.m.imbrie@gmail.com>
2017-02-28 14:00:42 -0600

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,20 @@
+import java.io.File;
 public class Driver {
 	public static void main(String[] args) {
 		CTController control = new CTController();
+		Simulator simulator = new Simulator();
+		Scanner userPrompt = new Scanner(System.in);
+		
+		if(!control.controlFromFile()){
+			while(true)
+				simulator.input(userPrompt.nextLine());
+		}
+		else{
+			System.out.print("Enter the full file directory: ");
+			try{
+				BufferedReader reader = new BufferedReader(new FileReader(userPrompt.nextLine()));
+			}
+			catch (FileNotFoundException e)
+		}
 	}
 }
