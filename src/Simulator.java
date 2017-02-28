6ca1eb806d49b4685b54f6baa73e216368109a4f
Robert imbrie <robert.m.imbrie@gmail.com>
2017-02-28 14:17:22 -0600

    Update Simulator.java

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,2 +1,13 @@
 public class Simulator(){
+  boolean powerOn;
+  
+  public Simulator(){
+  }
+  
+  public void input(String input){
+    
+    if(input.equalsIgnoreCase("POWER"))
+      powerOn = !powerOn;
+    if(input.equalsIgnoreCase("
+  }
 }
