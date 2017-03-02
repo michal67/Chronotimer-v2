9826825c9b31f1ae80b001ecd9d266ee9b47cd6b
Robert imbrie <robert.m.imbrie@gmail.com>
2017-03-02 15:46:23 -0600

    Update Simulator.java

--- src/Simulator.java
+++ src/Simulator.java
@@ -2,33 +2,61 @@ package src;
 
 public class Simulator{
   Chronotimer chronotimer;
-  boolean powerOn;
+  boolean powerOn, fileRead;
+  long time;
   
-  public Simulator(){
+  public Simulator(boolean fileRead){
     chronotimer = new Chronotimer();
     powerOn = false;
+    this.fileRead = fileRead;
   }
   
   public void input(String[] input){
+    //Special case for a file read where the command is prefixed by a time in HH:MM:SS.S formatting
+    if(fileRead){ 
+      time = parseTime(input[0]);         
+      input = System.arraycopy(input, 1, input, 0, input.length - 1);
+    }
+    else
+      time = System.nanoTime();
+    
     if(input[0].equalsIgnoreCase("POWER"))
       powerOn = !powerOn;
+    
     else if(powerOn){
       if(input[0].equalsIgnoreCase("TOG"))
-        chronotimer.toggle( Integer.parseInt( input[1]));
+        chronotimer.toggle( Integer.parseInt(input[1]), time);
+      
       else if(input[0].equalsIgnoreCase("TIME"))
-        chronotimer.setTime( input[1].split(":|."));
+        chronotimer.setTime( parseTime(input[1]), time);
+      
       else if(input[0].equalsIgnoreCase("TRIG"))
-        chronotimer.trigger( Integer.parseInt( input[1]));
+        chronotimer.trigger( Integer.parseInt(input[1]), time);
+      
       else if(input[0].equalsIgnoreCase("EVENT"))
-        chronotimer.setEvent( input[1]);
+        chronotimer.setEvent( input[1], time);
+      
       else if(input[0].equalsIgnoreCase("NEWRUN"))
-        chronotimer.newRun();
+        chronotimer.newRun(time);
+      
       else if(input[0].equalsIgnoreCase("ENDRUN"))
-        chronotimer.endRun();
+        chronotimer.endRun(time);
+      
       else if(input[0].equalsIgnoreCase("NUM"))
-        chronotimer.addCompetitor( Integer.parseInt(input[1]));
+        chronotimer.addCompetitor( Integer.parseInt(input[1]), time);
+      
       else if(input[0].equalsIgnoreCase("PRINT"))
-        System.out.println( chronotimer.print());
+        System.out.println( chronotimer.print(), time);
     }
+  }
+  
+  public long parseTime(String time){
+    String[] timeString = time.split(":|.");
+
+    return (Long.parseLong(timeString[0]) * 36000 //Convert to a single time unit (tenth-seconds), which is then converted to nanoseconds
+      + Long.parseLong(timeString[1]) * 600
+      + Long.parseLong(timeString[2]) * 10
+      + Long.parseLong(timeString[3]) *)
+      * 100000000;
   }
 }
