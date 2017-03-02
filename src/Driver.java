e298db12158c06b93b505e14bf557c483d37792b
Robert imbrie <robert.m.imbrie@gmail.com>
2017-03-02 15:39:05 -0600

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -2,24 +2,28 @@ import java.io.File;
 public class Driver {
 	public static void main(String[] args) {
 		CTController control = new CTController();
-		Simulator simulator = new Simulator();
 		Scanner userPrompt = new Scanner(System.in);
 		boolean fileBeenRead = false;
 		
 		if(!control.controlFromFile()){
+			Simulator simulator = new Simulator(false);
 			for(String nextLine = userPrompt.nextLine(); !nextLine.equalsIgnoreCase("EXIT"); nextLine = userPrompt.nextLine())
 				simulator.input(nextLine.split(" "));
 		}
-		while(!fileBeenRead){
-			System.out.print("Enter the full file directory: ");
-			try{
-				BufferedReader reader = new BufferedReader(new FileReader(userPrompt.nextLine()));
-				for(String nextLine = reader.nextLine(); nextLine != null; nextLine = reader.nextLine())
-					simulator.input(nextLine);
-				fileBeenRead = true;
+		else{
+			while(!fileBeenRead){
+				System.out.print("Enter the full file directory: ");
+				try{
+					BufferedReader reader = new BufferedReader(new FileReader(userPrompt.nextLine()));
+					Simulator simulator = new Simulator(true);
+				
+					for(String nextLine = reader.nextLine(); nextLine != null; nextLine = reader.nextLine())
+						simulator.input(nextLine.split(" "));
+					fileBeenRead = true;
+				}
+				catch (FileNotFoundException e)
+					System.out.println("Invalid filepath");
 			}
-			catch (FileNotFoundException e)
-				System.out.println("Invalid filepath");
 		}
 	}
 }
