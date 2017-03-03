0707c0c7800990ee1cb5c24c5ce58820d59e1cf7
Nicholas Michalowski <michal67@uwm.edu>
2017-03-02 18:56:16 -0600

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,28 +1,38 @@
+package src;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Driver {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		CTController control = new CTController();
 		Scanner userPrompt = new Scanner(System.in);
 		boolean fileBeenRead = false;
-		
-		if(!control.controlFromFile()){
+
+		if (!control.controlFromFile()) {
 			Simulator simulator = new Simulator(false);
-			for(String nextLine = userPrompt.nextLine(); !nextLine.equalsIgnoreCase("EXIT"); nextLine = userPrompt.nextLine())
+			for (String nextLine = userPrompt.nextLine(); !nextLine.equalsIgnoreCase("EXIT"); nextLine = userPrompt
+					.nextLine())
 				simulator.input(nextLine.split(" "));
-		}
-		else{
-			while(!fileBeenRead){
+		} else {
+			while (!fileBeenRead) {
 				System.out.print("Enter the full file directory: ");
-				try{
+				try {
 					BufferedReader reader = new BufferedReader(new FileReader(userPrompt.nextLine()));
 					Simulator simulator = new Simulator(true);
-				
-					for(String nextLine = reader.nextLine(); nextLine != null && !nextLine.equalsIgnoreCase("EXIT"); nextLine = reader.nextLine())
+
+					for (String nextLine = reader.readLine(); nextLine != null
+							&& !nextLine.equalsIgnoreCase("EXIT"); nextLine = reader.readLine())
 						simulator.input(nextLine.split(" "));
 					fileBeenRead = true;
-				}
-				catch (FileNotFoundException e)
+					reader.close();
+				} catch (FileNotFoundException e) {
 					System.out.println("Invalid filepath");
+				}
 			}
 		}
 	}
